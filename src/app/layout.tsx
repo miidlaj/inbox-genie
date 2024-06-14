93900929f88579a3ae44754e33506cb777bdcc3c
Muhammed Midlaj <70736601+miidlaj@users.noreply.github.com>
2024-06-14 04:22:09 +0000

    favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,7 @@ import { ThemeProvider } from "./components/theme-provider";
 import { ModeToggle } from "./components/theme-toggle";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { Toaster } from "@/components/ui/toaster";
+import Head from "next/head";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -35,7 +36,28 @@ export default async function RootLayout({
           fontSans.variable
         )}
       >
-
+        <Head>
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ffffff" />
+        </Head>
         <ThemeProvider attribute="class" defaultTheme="system">
           <SessionProvider session={session}>
             <TooltipProvider>{children}</TooltipProvider>
